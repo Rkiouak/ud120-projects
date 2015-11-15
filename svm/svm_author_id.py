ea2e16b5f0b3a598b6101c6b5bcf292c31184079
Matt Rkiouak <rkiouak@me.com>
2015-11-15 14:41:50 -0500

    finished lesson 2

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -26,5 +26,24 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 
 #########################################################
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
 
+from sklearn.svm import SVC
+clf = SVC(kernel='rbf', C=10000)
 
+t0 = time()
+print "starting training"
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
+
+from sklearn.metrics import accuracy_score
+print(accuracy_score(labels_test, pred))
+print("answer #10: ", pred[10])
+print("answer #26: ", pred[26])
+print("answer #50: ", pred[50])
+print("size of Chris answers: ", len([item for item in pred if item not in set([0])]))
